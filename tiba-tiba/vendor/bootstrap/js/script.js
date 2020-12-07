d9bfd8a7dad00d9d5d17d1dc5847c3b3c449dc2e
alihaja <70997226+alihaja@users.noreply.github.com>
2020-12-07 16:05:14 +0700

    transparant header dan responsive header tiba2event

---
+++ tiba-tiba/vendor/bootstrap/js/script.js
@@ -0,0 +1,13 @@
+var header = document.getElementById("header");
+var btns = header.getElementsByClassName("nav-link page-scroll");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
+
+
